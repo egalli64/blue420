0c505648959097a24129a29f80fd5da797e953f0
Angelica Iantorno <angelica.iantorno@yahoo.com>
2021-06-14 13:59:52 +0200

    user table and schema blue

---
+++ SQL/schemaBlue.sql
@@ -0,0 +1,9 @@
+drop user if exists blue;
+drop schema if exists blue;
+
+create user blue identified by 'password';
+create schema blue;
+grant all privileges on blue.* to blue;
+grant alter routine on blue.* to blue;
+
+use blue;
