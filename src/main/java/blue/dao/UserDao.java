4a0d78a0726176568c3256e4674dc1161e03270d
Giulia Cicatiello <giuliacicatiello9494@libero.it>
2021-06-16 18:12:51 +0200

    Changes Login.java and UserDao

--- src/main/java/blue/dao/UserDao.java
+++ src/main/java/blue/dao/UserDao.java
@@ -2,6 +2,7 @@ package blue.dao;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,6 +17,7 @@ import org.slf4j.LoggerFactory;
 public class UserDao implements AutoCloseable {
 	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 	private static final String GET_ALL = "SELECT user_id, username, password FROM users";
+	private static final String GET_USER = "SELECT user_id, username, password FROM users where username = ? and password = ?";
 	private Connection conn;
 
 	public UserDao(DataSource ds) {
@@ -44,6 +46,26 @@ public class UserDao implements AutoCloseable {
 		}
 
 		return results;
+	}
+	
+	public User getUser(String username, String password) {
+		log.trace("called");
+
+		try (PreparedStatement stmt = conn.prepareStatement(GET_USER)) {
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return new User(rs.getInt(1), rs.getString(2), rs.getString(3));
+			} else {
+				return null;
+			}
+		} catch (SQLException se) {
+			log.error("Can't get films: " + se.getMessage());
+			throw new IllegalStateException("Database issue " + se.getMessage());
+		}
+
 	}
 
 	@Override
--- src/main/java/blue/srv/Login.java
+++ src/main/java/blue/srv/Login.java
@@ -1,17 +1,38 @@
 package blue.srv;
 
 import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import blue.dao.User;
+import blue.dao.UserDao;
+
 
 @WebServlet("/login")
 @SuppressWarnings("serial")
 public class Login extends HttpServlet {
+	@Resource(name = "jdbc/blue")
+	private DataSource ds;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not implemented yet");
+    	String username = request.getParameter("username");
+    	String password = request.getParameter("password");
+    	if (username == null || password == null) {
+    	} else {
+    		try (UserDao dao = new UserDao(ds)) {
+    			request.setAttribute("users", dao.getUser(username, password));
+    	}
+    	}
+       request.setAttribute("logged", new User("?","?"));
+       request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+        
     }
 }
